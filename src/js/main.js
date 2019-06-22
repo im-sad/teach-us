48864664134b4d4a880b8b68debf693fa598d248
Andrew <im-sad@ya.ru>
2019-06-22 08:08:19 +0300

    Add show-pass JS plugin

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,2 @@
 @@include('./ripple-effect.js')
+@@include('./pass-eye.js')
---
+++ src/js/pass-eye.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
+  (function () {
+    var passEyeBtns = document.getElementsByClassName('js-pass-eye');
+
+    for (var i = 0; i < passEyeBtns.length; i++) {
+      passEyeBtns[i].addEventListener('click', function(e) {
+        var field = e.target.parentNode;
+        var input = field.getElementsByTagName('input')[0];
+
+        if (!input) return;
+
+        if (input.getAttribute('type') == 'password') {
+          field.classList.add('is-pass-show');
+          input.setAttribute('type', 'text');
+        } else {
+          field.classList.remove('is-pass-show');
+          input.setAttribute('type', 'password');
+        }
+      });
+    }
+
+  }());
+});
